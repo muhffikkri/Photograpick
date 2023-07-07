67e3685d8c48a745bcbb1e6b315a4c4addae1191
Nexvaulus <moehammad.fikr@gmail.com>
2023-07-07 09:57:47 +0700

    Commit pc

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,6 @@
 import 'regenerator-runtime';
-import '../styles/style.css';
-import '../styles/responsive.css';
+// import '../styles/style.css';
+// import '../styles/responsive.css';
 import App from './views/app';
 import swRegister from './utils/sw-register';
 
