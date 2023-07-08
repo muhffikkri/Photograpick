c9d16cb59389cde869a8ed9f1e69eddfd2228f8b
Nexvaulus <moehammad.fikr@gmail.com>
2023-07-08 09:22:08 +0700

    Navbar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,6 +5,10 @@ import App from './views/app';
 // Import our custom CSS
 import '../scss/styles.scss';
 
+// Import all of Bootstrap's JS
+// eslint-disable-next-line no-unused-vars
+import * as bootstrap from 'bootstrap';
+
 // import custom components
 import './views/components/app-bar';
 import './views/components/foot-note';
